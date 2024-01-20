9905111e9a11159e83bd355fabf3c7f18474928d
Doug Slater <doug@slater.dev>
2024-01-20 11:52:27 -0500

    Jot down some ideas

---
+++ nreq.cli/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace nreq.cli;
+
+public static class Program
+{
+  public static void Main()
+  {
+    // Requirements:
+    // Reflect over all instances of RequirementAttribute
+    // For each field of a req, ensure there is a corresponding SatisfiesAttribute
+    // All child reqs of a req must be satisfied, in addition to its own fields
+    // Else throw compile time error
+
+    // Risks:
+    // Accumulate risks and throw compile time error if the value is too high
+
+    // Notes:
+    // Throw compile time error if any note does not have a corresponding description
+
+    // Requirements must be satisfied; risks must be mitigated
+  }
+}
---
+++ nreq.tests/Brainstorming.cs
@@ -0,0 +1,16 @@
+namespace nreq.tests;
+
+[TestFixture]
+public class Brainstorming
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
